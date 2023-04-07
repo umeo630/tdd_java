ce1d18921fd5f566bd6be0ec668eaf64aa7247a2
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-04-07 22:45:21 +0900

    first commit

---
+++ src/test/java/com/example/tdd_java/TddJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdd_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
