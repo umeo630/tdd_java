a3bc7dd10dbad9b04a5554a99532911dbe5faf92
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-04-10 09:33:08 +0900

    5章　原則をあえて破るとき 正しい設計を正しいタイミングで行う 動かしてから正しくする

---
+++ src/test/java/com/example/tdd_java/money/Franc.java
@@ -0,0 +1,15 @@
+package com.example.tdd_java.money;
+
+public class Franc {
+    private int amount;
+    Franc(int amount) {
+        this.amount = amount;
+    }
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
