4dfef32c5c13ffc4c25228ae4f0f9a56fd8ee678
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-04-13 22:56:22 +0900

    9章 歩幅の調整

--- src/test/java/com/example/tdd_java/money/Franc.java
+++ src/test/java/com/example/tdd_java/money/Franc.java
@@ -1,10 +1,10 @@
 package com.example.tdd_java.money;
 
 public class Franc extends Money {
-    Franc(int amount) {
-        this.amount = amount;
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
     Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 }
