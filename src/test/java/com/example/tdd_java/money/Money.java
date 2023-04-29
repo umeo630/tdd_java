d37b103eca159e346a14fa7f38212a28e0cb5d12
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-04-29 15:22:51 +0900

    15章 テスト任せとコンパイラ任せ ・MoneyからExpressionへの一般化をできるところまで ・こうなったら良いというテスト→まず一歩で動かせるところまでそのテストを後退 ・一般化作業を末端から行い頂点（テストケース）まで到達させた ・変更の際コンパイラに従い、1つずつ対応

--- src/test/java/com/example/tdd_java/money/Money.java
+++ src/test/java/com/example/tdd_java/money/Money.java
@@ -2,7 +2,7 @@ package com.example.tdd_java.money;
 public class Money implements Expression {
     protected int amount;
     protected String currency;
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(multiplier * amount, currency);
     }
 
@@ -32,7 +32,7 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
