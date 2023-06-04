6055bf234fbb194c1cbfe9a54c29af70c9c6f383
Hikaru-Umezawa <hikaru.19970630@gmail.com>
2023-06-04 13:21:41 +0900

    update: テスト ・パラメータテストを使用 ・ValueSource

--- src/test/java/com/example/tdd_java/parameterized/SeasonTest.java
+++ src/test/java/com/example/tdd_java/parameterized/SeasonTest.java
@@ -2,7 +2,8 @@ package com.example.tdd_java.parameterized;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,41 +16,37 @@ class SeasonTest {
     }
 
     @DisplayName("3~5月の場合、春を返すか")
-    @Test
-    public void testGetSeasonReturnsSpringForMarchToMay() {
-        assertEquals("春", season.getSeason(3));
-        assertEquals("春", season.getSeason(4));
-        assertEquals("春", season.getSeason(5));
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5})
+    public void testGetSeasonReturnsSpringForMarchToMay(int month) {
+        assertEquals("春", season.getSeason(month));
     }
 
     @DisplayName("6~8月の場合、夏を返すか")
-    @Test
-    public void testGetSeasonReturnsSummerForJuneToAugust() {
-        assertEquals("夏", season.getSeason(6));
-        assertEquals("夏", season.getSeason(7));
-        assertEquals("夏", season.getSeason(8));
+    @ParameterizedTest
+    @ValueSource(ints = {6, 7, 8})
+    public void testGetSeasonReturnsSummerForJuneToAugust(int month) {
+        assertEquals("夏", season.getSeason(month));
     }
 
     @DisplayName("9~11月の場合、秋を返すか")
-    @Test
-    public void testGetSeasonReturnsAutumnForSeptemberToNovember() {
-        assertEquals("秋", season.getSeason(9));
-        assertEquals("秋", season.getSeason(10));
-        assertEquals("秋", season.getSeason(11));
+    @ParameterizedTest
+    @ValueSource(ints = {9, 10, 11})
+    public void testGetSeasonReturnsAutumnForSeptemberToNovember(int month) {
+        assertEquals("秋", season.getSeason(month));
     }
 
     @DisplayName("12~2月の場合、冬を返すか")
-    @Test
-    public void testGetSeasonReturnsWinterForDecemberToFebruary() {
-        assertEquals("冬", season.getSeason(12));
-        assertEquals("冬", season.getSeason(1));
-        assertEquals("冬", season.getSeason(2));
+    @ParameterizedTest
+    @ValueSource(ints = {12, 1, 2})
+    public void testGetSeasonReturnsWinterForDecemberToFebruary(int month) {
+        assertEquals("冬", season.getSeason(month));
     }
 
     @DisplayName("不正な値の場合、`不正な値です`を返すか")
-    @Test
-    public void testGetSeasonReturnsInvalidValueForInvalidValue() {
-        assertEquals("不正な値です", season.getSeason(0));
-        assertEquals("不正な値です", season.getSeason(13));
+    @ParameterizedTest
+    @ValueSource(ints = {0, 13})
+    public void testGetSeasonReturnsInvalidValueForInvalidValue(int month) {
+        assertEquals("不正な値です", season.getSeason(month));
     }
 }
